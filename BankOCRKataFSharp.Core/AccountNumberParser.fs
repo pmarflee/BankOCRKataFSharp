824892a6b86307e8d01b6c9893f15533689fe35d
Paul Marfleet <paul.marfleet@gmail.com>
2014-12-07 21:58:58 +0000

    Implemented parsing account numbers

---
+++ BankOCRKataFSharp.Core/AccountNumberParser.fs
@@ -0,0 +1,40 @@
+﻿namespace BankOCRKataFSharp.Core
+
+open System
+
+module AccountNumberParser =
+
+    open System
+
+    module private Parser =
+
+        let numbers = [|
+            " _ ","| |","|_|";
+            "   ", "  |", "  |";
+            " _ "," _|","|_ ";
+            " _ "," _|"," _|";
+            "   ","|_|","  |";
+            " _ ","|_ "," _|";
+            " _ ","|_ ","|_|";
+            " _ ","  |","  |";
+            " _ ","|_|","|_|";
+            " _ ","|_|"," _|"
+        |]
+
+    let Parse (input : string) =
+        let lines = input.Split([|Environment.NewLine|], StringSplitOptions.RemoveEmptyEntries)
+        let rec parseline (state : string list) (current : char list) (remaining : char list) =
+            match remaining with
+            | [] -> state
+            | hd :: tl ->
+                let new_current = hd :: current
+                match new_current with
+                | [_;_;_] -> parseline (new String(new_current |> List.rev |> List.toArray) :: state) [] tl                    
+                | _ -> parseline state new_current tl
+        let segments = lines 
+                        |> Seq.map (fun line -> parseline [] [] (line.ToCharArray() |> Array.toList)) 
+                        |> Seq.toList
+        let parsednumbers = List.zip3 segments.[0] segments.[1] segments.[2] 
+                            |> List.rev
+                            |> List.map (fun parsed -> (Parser.numbers |> Array.findIndex (fun number -> number = parsed)).ToString())
+        Int32.Parse(String.Join("", parsednumbers))
---
+++ BankOCRKataFSharp.Tests/AccountNumberParserTests.fs
@@ -0,0 +1,18 @@
+﻿namespace BankOCRKataFSharp.Tests
+
+open Xunit
+open Xunit.Extensions
+open FsUnit.Xunit
+open BankOCRKataFSharp.Core
+
+module AccountNumberParserTests =
+
+    [<Theory>]
+    [<InlineData("    _  _     _  _  _  _  _ \r\n  | _| _||_||_ |_   ||_||_|\r\n  ||_  _|  | _||_|  ||_| _|", 123456789)>]
+    [<InlineData("    _  _     _  _  _  _  _ \r\n  | _| _||_||_ |_   ||_||_|\r\n  ||_  _|  | _||_|  ||_||_|", 123456788)>]
+    [<InlineData("    _  _     _  _  _  _  _ \r\n  | _| _||_||_ |_   ||_||_|\r\n  | _| _|  | _||_|  ||_||_|", 133456788)>]
+    [<InlineData("    _  _     _  _  _  _  _ \r\n  | _| _||_||_ |_   ||_|| |\r\n  | _| _|  | _||_|  ||_||_|", 133456780)>]
+    let ``AccountNumberParser.Parse Returns Correct Result`` entry expected =
+        entry
+        |> AccountNumberParser.Parse
+        |> should equal expected
